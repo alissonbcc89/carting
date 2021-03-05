abc7e6d29b889f1517fa5c3e2667a716ff77a4a6
alissnbcc89 <alissnbcc89@gmail.com>
2021-03-05 05:22:34 -0300

    sseconde commit

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,7 +10,10 @@
 // Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
 // about supported directives.
 //
+//= require jquery
+//= require jquery-ui/effects/effect-blind
 //= require rails-ujs
 //= require activestorage
 //= require turbolinks
 //= require_tree .
+
